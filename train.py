22ba5a6593ece291378facf906046f54ba02173e
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-10-20 12:17:41 +0200

    Update

--- train.py
+++ train.py
@@ -13,22 +13,15 @@ from stable_baselines3.common.monitor import Monitor
 @hydra.main("configs", "base", version_base="1.1")
 def main(cfg: DictConfig) -> float:
     printr(cfg)
-    env_id = cfg.env_id
-    model_fn = cfg.model_fn
-    log_dir = cfg.log_dir
-    total_timesteps = cfg.total_timesteps
-    n_eval_episodes = cfg.n_eval_episodes
-    verbose = cfg.verbose
-    seed = cfg.seed
 
-    env = gym.make(env_id)
-    model = SAC("MlpPolicy", env, verbose=verbose, tensorboard_log=log_dir, seed=seed)
-    model.learn(total_timesteps=total_timesteps)
-    model.save(model_fn)
+    env = gym.make(cfg.env_id)
+    model = SAC("MlpPolicy", env, verbose=cfg.verbose, tensorboard_log=cfg.log_dir, seed=cfg.seed)
+    model.learn(total_timesteps=cfg.total_timesteps)
+    model.save(cfg.model_fn)
 
     # Evaluate
-    env = Monitor(gym.make(env_id))
-    means, stds = evaluate_policy(model, env, n_eval_episodes=n_eval_episodes)
+    env = Monitor(gym.make(cfg.env_id))
+    means, stds = evaluate_policy(model, env, n_eval_episodes=cfg.n_eval_episodes)
     performance = np.mean(means)
 
     return performance
